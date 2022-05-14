8b3af8e8cf81c8ce9c2a649a765843f698d4998b
go4654 <go4654@naver.com>
2022-05-14 20:21:04 +0900

    deploy-4

--- src/apollo.js
+++ src/apollo.js
@@ -33,11 +33,15 @@ export const offDarkMode = () => {
 //   uri: "http://192.168.0.10:4000/graphql",
 // });
 
+// const uploadLink = createUploadLink({
+//   uri:
+//     process.env.NODE_ENV === "production"
+//       ? "https://cook-recipe-backend.herokuapp.com/graphql"
+//       : "http://192.168.0.10:4000/graphql",
+// });
+
 const uploadLink = createUploadLink({
-  uri:
-    process.env.NODE_ENV === "production"
-      ? "https://cook-recipe-backend.herokuapp.com/graphql"
-      : "http://192.168.0.10:4000/graphql",
+  uri: "http://192.168.0.10:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
